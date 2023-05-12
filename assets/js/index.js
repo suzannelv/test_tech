6601af2d57f90d03d1e12e9bb736c85bd99865c2
suzannelv <lvmeijuan9011@gmail.com>
2023-05-12 22:29:32 +0200

    bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,12 +11,12 @@ class Patients {
 const avatarPath = "assets/img/avatars/"
 // Instancier la class Person
 
-let person1 = new Patients(avatarPath + "patient_1.png", "Cécile Moulin", 81);
-let person2 = new Patients(avatarPath + "patiente_2_4.png", "Juliette Masselis", 39);
-let person3 = new Patients(avatarPath + "patient_3.png", "René Guitay", 81);
-let person4 = new Patients(avatarPath + "patiente_2_4.png", "Clara Camalet", 31);
-let person5 = new Patients(avatarPath + "patiente_5.png", "Lola Dufour", 81);
-let person6 = new Patients(avatarPath + "patient_6.png", "Etienne Morié", 81);
+let person1 = new Patients(avatarPath + "patiente_cecile.png", "Cécile Moulin", 81);
+let person2 = new Patients(avatarPath + "patiente_ju_clara.png", "Juliette Masselis", 39);
+let person3 = new Patients(avatarPath + "patient_rene.png", "René Guitay", 81);
+let person4 = new Patients(avatarPath + "patiente_ju_clara.png", "Clara Camalet", 31);
+let person5 = new Patients(avatarPath + "patiente_lola.png", "Lola Dufour", 81);
+let person6 = new Patients(avatarPath + "patient_etienne.png", "Etienne Morié", 81);
 
 // tableau des personnes
 const personsArr = [person1, person2, person3, person4, person5, person6]
