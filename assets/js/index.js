19a3ebeea9696c0a84769bf54016e56160d45331
suzannelv <lvmeijuan9011@gmail.com>
2023-05-12 22:17:38 +0200

    avatar

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ class Patients {
 }
 
 
-const avatarPath = "/assets/img/avatars/"
+const avatarPath = "assets/img/avatars/"
 // Instancier la class Person
 
 let person1 = new Patients(avatarPath + "patient_1.png", "Cécile Moulin", 81);
@@ -49,28 +49,28 @@ for(let i = 0; i < personsArr.length; i++){
 
           <div class="info ms-2">
             <div class="icon me-2">
-              <img src="./assets/img/icons/tel.png" alt="">
+              <img src="assets/img/icons/tel.png" alt="">
             </div>
             <p>04 77 36 65 95</p>
           </div>
 
           <div class="info align-items-center ms-2">
             <div class="icon me-2">
-              <img src="./assets/img/icons/mail.png" alt="">
+              <img src="assets/img/icons/mail.png" alt="">
             </div>
             <p>cecilemoulin@gmail.com</p>
           </div>
 
           <div class="info ms-2">
             <div class="icon me-2">
-              <img src="./assets/img/icons/adress.png" alt="">
+              <img src="assets/img/icons/adress.png" alt="">
             </div>
             <p>16 rue Maurice Bouchor 68007 Lyon</p>
           </div>
 
           <div class="info align-items-center ms-2">
             <div class=" me-2">
-              <img src="./assets/img/icons/Union_plus.png" alt="">
+              <img src="assets/img/icons/Union_plus.png" alt="">
             </div>
             <p>voir ses ordonnances</p>
           </div>
