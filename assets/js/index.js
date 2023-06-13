d67533c76826211122544e61d305eee293f31de9
suzannelv <lvmeijuan9011@gmail.com>
2023-06-13 16:10:56 +0200

    amélioration 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,12 +11,12 @@ class Patients {
 const avatarPath = "assets/img/avatars/"
 // Instancier la class Person
 
-let person1 = new Patients(avatarPath + "patiente_cecile.png", "Cécile Moulin", 81);
-let person2 = new Patients(avatarPath + "patiente_ju_clara.png", "Juliette Masselis", 39);
-let person3 = new Patients(avatarPath + "patient_rene.png", "René Guitay", 81);
-let person4 = new Patients(avatarPath + "patiente_ju_clara.png", "Clara Camalet", 31);
-let person5 = new Patients(avatarPath + "patiente_lola.png", "Lola Dufour", 81);
-let person6 = new Patients(avatarPath + "patient_etienne.png", "Etienne Morié", 81);
+let person1 = new Patients(avatarPath + "patiente-cecile.svg", "Cécile Moulin", 81);
+let person2 = new Patients(avatarPath + "patiente-clara.svg", "Juliette Masselis", 39);
+let person3 = new Patients(avatarPath + "patient-rene.svg", "René Guitay", 81);
+let person4 = new Patients(avatarPath + "patiente-clara.svg", "Clara Camalet", 31);
+let person5 = new Patients(avatarPath + "patiente-lola.svg", "Lola Dufour", 81);
+let person6 = new Patients(avatarPath + "patient-etienne.svg", "Etienne Morié", 81);
 
 // tableau des personnes
 const personsArr = [person1, person2, person3, person4, person5, person6]
@@ -32,7 +32,7 @@ for(let i = 0; i < personsArr.length; i++){
   let cardClass = count % 2 === 0 ? "even" : "odd";
   // créer le template pour les cartes de patients
   html += `
-    <div class="col info d-flex flex-column align-items-center">
+    <div class="col info d-flex flex-column align-items-center mx-1">
       <div class="avatar-patient">
         <img src="${person.avatar}" alt="avatar de patient ${person.name}">
       </div>
@@ -49,28 +49,28 @@ for(let i = 0; i < personsArr.length; i++){
 
           <div class="info ms-2">
             <div class="icon me-2">
-              <img src="assets/img/icons/tel.png" alt="">
+              <img src="assets/img/icons/tel.svg" alt="">
             </div>
             <p>04 77 36 65 95</p>
           </div>
 
           <div class="info align-items-center ms-2">
             <div class="icon me-2">
-              <img src="assets/img/icons/mail.png" alt="">
+              <img src="assets/img/icons/mail.svg" alt="">
             </div>
             <p>cecilemoulin@gmail.com</p>
           </div>
 
           <div class="info ms-2">
             <div class="icon me-2">
-              <img src="assets/img/icons/adress.png" alt="">
+              <img src="assets/img/icons/adress.svg" alt="">
             </div>
             <p>16 rue Maurice Bouchor 68007 Lyon</p>
           </div>
 
           <div class="info align-items-center ms-2">
             <div class=" me-2">
-              <img src="assets/img/icons/Union_plus.png" alt="">
+              <img src="assets/img/icons/union-plus.svg" alt="">
             </div>
             <p>voir ses ordonnances</p>
           </div>
